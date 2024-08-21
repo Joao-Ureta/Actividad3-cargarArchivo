f02f7f5506fe0dcbe7f5bc7c8d2a205fee82f325
Joao Ureta <joao.ureta@gmail.com>
2024-08-20 21:10:46 -0400

    Agregar archivos de proyecto.

---
+++ Actividad3-cargarArchivo/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCargar = New System.Windows.Forms.Button()
+        Me.txtMostrar = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnCargar
+        '
+        Me.btnCargar.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCargar.Location = New System.Drawing.Point(163, 125)
+        Me.btnCargar.Name = "btnCargar"
+        Me.btnCargar.Size = New System.Drawing.Size(126, 47)
+        Me.btnCargar.TabIndex = 1
+        Me.btnCargar.Text = "Cargar"
+        Me.btnCargar.UseVisualStyleBackColor = True
+        '
+        'txtMostrar
+        '
+        Me.txtMostrar.Location = New System.Drawing.Point(78, 231)
+        Me.txtMostrar.Multiline = True
+        Me.txtMostrar.Name = "txtMostrar"
+        Me.txtMostrar.Size = New System.Drawing.Size(292, 49)
+        Me.txtMostrar.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 438)
+        Me.Controls.Add(Me.txtMostrar)
+        Me.Controls.Add(Me.btnCargar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCargar As Button
+    Friend WithEvents txtMostrar As TextBox
+End Class
---
+++ Actividad3-cargarArchivo/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub btnCargar_Click(sender As Object, e As EventArgs) Handles btnCargar.Click
+
+        'Dim rutaArchivo As String = System.IO.Path.Combine(Application.StartupPath "carga.txt")
+        'C:\Users\Joao\source\repos\Actividad3-cargarArchivo
+
+        Dim rutaArchivo As String = "C:\Users\Joao\source\joaoUreta.txt"
+        Try
+            If My.Computer.FileSystem.FileExists(rutaArchivo) Then
+                Dim contenido As String = My.Computer.FileSystem.ReadAllText(rutaArchivo)
+                txtMostrar.Text = contenido
+                MessageBox.Show("Archivo cargado con exito")
+
+            Else
+                MessageBox.Show("El archivo no fue encontrado")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error al cargar el archivo")
+        End Try
+
+    End Sub
+End Class
